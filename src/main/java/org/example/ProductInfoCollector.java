53ca09df2f6269f36ea0286baddb338292dcb92a
q56881 <artem56881@gmail.com>
2024-11-27 18:07:06 +0500

    Добавил цену по озон карте

--- src/main/java/org/example/ProductInfoCollector.java
+++ src/main/java/org/example/ProductInfoCollector.java
@@ -16,11 +16,19 @@ public class ProductInfoCollector {
             // Parse the HTML content with Jsoup
             Document document = Jsoup.parse(htmlContent);
 
+            Element oCardPriceElement = document.selectFirst("span.m7s_27.sm5_27");
             Element scriptElement = document.selectFirst("script[type=\"application/ld+json\"]");
-            System.out.println(scriptElement.html());
+//            System.out.println(scriptElement.html());
+//            System.out.println(oCardPriceElement.html());
 
+            assert scriptElement != null;
             String ldJson = scriptElement.html();
 
+            String oCardPrice = null;
+
+            if (oCardPriceElement != null) {
+                oCardPrice = oCardPriceElement.html();
+            }
             String basePrice = ldJson.substring(ldJson.indexOf("\"price\":\"")+9, ldJson.indexOf("\",\"priceC"));
             String itemId = ldJson.substring(ldJson.indexOf("\"sku\":\"")+7, ldJson.lastIndexOf("\"}"));
             String itemName = ldJson.substring(ldJson.indexOf("\"name\":\"")+8, ldJson.lastIndexOf("\",\"offe"));
@@ -30,6 +38,7 @@ public class ProductInfoCollector {
             productInfo.put("item_id", itemId);
             productInfo.put("item_name", itemName);
             productInfo.put("item_rating", itemRating);
+            productInfo.put("ozon_card_price", oCardPrice);
 
         } catch (Exception e) {
             System.err.println("Error parsing HTML content: " + e.getMessage());
